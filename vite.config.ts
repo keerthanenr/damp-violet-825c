2b402ed4cd614d0b18124fa199c15406321f3daf
Keerthanen <keerthanenr@live.co.uk>
2025-04-20 20:57:10 +0100

    Test

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
+import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 import { cloudflare } from "@cloudflare/vite-plugin";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  plugins: [react(), tailwindcss(), tsconfigPaths(), cloudflare()],
+  build: {
+    target: "esnext",
+    modulePreload: {
+      polyfill: true,
+    },
+    rollupOptions: {
+      output: {
+        format: "es",
+        entryFileNames: "assets/[name]-[hash].js",
+        chunkFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash].[ext]",
+      },
+    },
+  },
 });
